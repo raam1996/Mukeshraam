ed0f2484a9640c25cb0d03f077d5c4a4e64667cc
Mukeshraam1996 <115001948+Mukeshraam1996@users.noreply.github.com>
2022-12-25 08:07:43 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mukeshraam
---
+++ src/org/details/ClientInfo.java
@@ -0,0 +1,15 @@
+package org.details;
+
+public class ClientInfo {
+	public ClientInfo() {
+		System.out.println("MUKESHRAAM");
+	}
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClientInfo c = new ClientInfo();
+		
+	}
+
+}
